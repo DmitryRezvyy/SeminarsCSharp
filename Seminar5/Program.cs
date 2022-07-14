5b55160dbfa9ddee34b3f88a4f4052c2eea0f415
Dmitry_Rezvyy <viggendr@yandex.ru>
2022-07-14 22:40:03 +0300

    Добавлена задача №34 к семинару 5

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача №34 - Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int EvenItemCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+void ShowArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1)
+        Console.Write(array[i] +", ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(a);
+ShowArray(myArray);
+Console.WriteLine($" -> {EvenItemCount(myArray)}");
