c0c4544053f4679f2bcb2f666f91ad5e3774ea5f
Dmitry_Rezvyy <viggendr@yandex.ru>
2022-07-19 22:23:24 +0300

    Добавлена задача №50 к семинару 7

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача №47 Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+/*
 Console.WriteLine("Укажите размер массива");
 Console.WriteLine("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
@@ -30,3 +30,45 @@ void PrintArray(double [,] array)
 }
 double[,] myArray = Create2DimArray(rows, columns);
 PrintArray(myArray);
+*/
+
+//Задача №50 Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("Введите номер строки: ");
+int row = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int column = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine();
+int rows = new Random().Next(2, 6);
+int columns = new Random().Next(2, 6);
+int[,] Create2DimArray(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray[i, j] = new Random().Next(101);
+        }
+    }
+    return newArray;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = Create2DimArray(rows, columns);
+PrintArray(myArray);
+if (row <= rows && column <= columns)
+    Console.Write($"\n[{row},{column}] -> {myArray[row-1, column-1]}");
+else
+    Console.Write($"\n[{row},{column}] -> Такого элемента в массиве нет");
