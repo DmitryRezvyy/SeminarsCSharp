8b01b466454caeea8ad8ced651ab258a50ed5a2e
Dmitry_Rezvyy <viggendr@yandex.ru>
2022-07-20 23:37:19 +0300

    Добавлена задача №56 к семинару 8

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача №54 Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-
+/*
 Console.WriteLine("Укажите размер массива");
 Console.WriteLine("Введите количество строк: ");
 int rows = Convert.ToInt16(Console.ReadLine());
@@ -54,3 +54,53 @@ int[,] myArray = Create2DimArray(rows, columns);
 PrintArray(myArray);
 myArray = SortMaxToMinElementsInRows(myArray);
 PrintArray(myArray);
+*/
+
+//Задача №56 Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Укажите размер массива: ");
+int size = Convert.ToInt16(Console.ReadLine());
+int[,] Create2DimArray(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray[i, j] = new Random().Next(101);
+        }
+    }
+    return newArray;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int MinSumRowNumber(int[,] array)
+{
+    int[] sum = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum[i] = sum[i] + array[i, j];
+        }
+    }
+    for (int t = 0; t < sum.Length; t++)
+    {
+        Console.Write(sum[t] + " ");
+    }
+    int rowNumber = Array.IndexOf(sum, sum.Min());
+    return rowNumber + 1;
+}
+int[,] myArray = Create2DimArray(size, size);
+PrintArray(myArray);
+Console.WriteLine($"Строка с минимальной суммой элементов: {MinSumRowNumber(myArray)} строка");
