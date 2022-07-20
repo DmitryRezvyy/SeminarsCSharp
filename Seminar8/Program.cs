6031ee4524181064f304532b221f07338e9e9992
Dmitry_Rezvyy <viggendr@yandex.ru>
2022-07-21 00:42:48 +0300

    Добавлена задача №58 к семинару 8

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -57,7 +57,7 @@ PrintArray(myArray);
 */
 
 //Задача №56 Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
+/*
 Console.WriteLine("Укажите размер массива: ");
 int size = Convert.ToInt16(Console.ReadLine());
 int[,] Create2DimArray(int m, int n)
@@ -104,3 +104,64 @@ int MinSumRowNumber(int[,] array)
 int[,] myArray = Create2DimArray(size, size);
 PrintArray(myArray);
 Console.WriteLine($"Строка с минимальной суммой элементов: {MinSumRowNumber(myArray)} строка");
+*/
+
+//Задача №58 Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.WriteLine("Укажите число строк матрицы А: ");
+int rowsA = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Укажите число столбцов матрицы А (строк матрицы B): ");
+int columnsA = Convert.ToInt16(Console.ReadLine());
+int rowsB = columnsA;
+Console.WriteLine("Укажите число столбцов матрицы B: ");
+int columnsB = Convert.ToInt16(Console.ReadLine());
+int[,] Create2DimArray(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return newArray;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MatrixMultiplication(int[,] m, int[,] n)
+{
+    int[,] newArray = new int[m.GetLength(0), n.GetLength(1)];
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < m.GetLength(1); k++)
+            {
+                newArray[i, j] = newArray[i, j] + m[i, k] * n [k, j];
+            }
+            
+        }
+    }
+    return newArray;
+}
+int[,] myArrayA = Create2DimArray(rowsA, columnsA);
+int[,] myArrayB = Create2DimArray(rowsB, columnsB);
+int[,] myArrayC = new int[rowsA, columnsB];
+myArrayC = MatrixMultiplication(myArrayA, myArrayB);
+Console.WriteLine("Матрица А:");
+PrintArray(myArrayA);
+Console.WriteLine("Матрица B:");
+PrintArray(myArrayB);
+Console.WriteLine("Произведение матриц:");
+PrintArray(myArrayC);
