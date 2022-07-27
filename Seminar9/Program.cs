df33d44c56a6624aedfef90e86a6914e2eba4821
Dmitry_Rezvyy <viggendr@yandex.ru>
2022-07-27 21:20:04 +0300

    Добавлена задача №64 к семинару 9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №64 Задайте значение N. Напишите программу, которая найдет факториал числа N рекурсивным методом.
+
+Console.WriteLine("Введите число, факториал которого необходимо вычислить:");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int Factorial(int n)
+{
+if (n == 0 || n == 1)
+return 1;
+else
+    n = n * Factorial(--n);
+return n;
+}
+
+Console.WriteLine($"N = {value} -> {Factorial(value)}");
+
+
