9e1a90a8b72d3b2e527dc9631e1c1cc1aa9d6e9e
Dmitry_Rezvyy <viggendr@yandex.ru>
2022-07-27 22:35:05 +0300

    Добавлены задачи к семинару 9

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача №64 Задайте значение N. Напишите программу, которая найдет факториал числа N рекурсивным методом.
-
+/*
 Console.WriteLine("Введите число, факториал которого необходимо вычислить:");
 int value = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
@@ -13,5 +13,20 @@ return n;
 }
 
 Console.WriteLine($"N = {value} -> {Factorial(value)}");
+*/
 
+// Задача №66 Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+Console.WriteLine("Введите начальное значение диапазона натуральных чисел");
+int valueM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное значение диапазона натуральных чисел");
+int valueN = Convert.ToInt32(Console.ReadLine());
+int Sum(int m, int n)
+{
+    if (n - m > 0)
+    {
+        m = m + Sum(++m, n);
+    }
+   return m;
+}
+Console.WriteLine($"M = {valueM}; N = {valueN} -> {Sum(valueM, valueN)}");
